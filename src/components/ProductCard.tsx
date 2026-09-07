27d01221baa526125a19a80b506075b17f8c3d12
Fox Scarlett <foxontheone@gmail.com>
2026-09-06 20:57:49 -0700

    data json, map product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,17 +1,29 @@
 import styles from '../styles/ProductCard.module.css'
 
-function ProductCard() {
+function ProductCard({
+  product,
+}: {
+  product: {
+    id: number
+    name: string
+    description: string
+    image: string
+    tags?: string[]
+    price: number
+  }
+}) {
+  const productImage = product.image || '/hello-kitty-wall-clock-small.png' // Default image if none provided
   return (
     <div className={styles['product-card']}>
       <div className={styles['product-card-header']}>
-        <p>pills and badges</p>
+        <p>{product.tags?.join(', ')}</p>
       </div>
       <div className={styles['product-card-body']}>
-        <h2>product name</h2>
-        <p>product description</p>
+        <h2>{product.name}</h2>
+        <p>{product.description}</p>
       </div>
       <div className={styles['product-card-image-wrapper']}>
-        <img src='/hello-kitty-wall-clock-small.png' alt='default product' />
+        <img src={productImage} alt={product.name} />
       </div>
     </div>
   )
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,14 +1,14 @@
 import ProductCard from './ProductCard'
 import styles from '../styles/Products.module.css'
 
+import productList from '../data/product_list.json'
+
 function Products() {
   return (
     <div className={styles['products-wrapper']}>
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      {productList.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
     </div>
   )
 }
