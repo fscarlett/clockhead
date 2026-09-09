a4a4e9ffdfccdc910597eeb1ed803d253b0d79f9
Fox Scarlett <foxontheone@gmail.com>
2026-09-08 23:43:37 -0700

    post card with random-word-api call for title

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,13 +1,28 @@
 import { Link } from 'react-router'
+import { useState, useEffect } from 'react'
 import styles from '../styles/PostCard.module.css'
 
-async function PostCard({ id }: { id: number }) {
+function PostCard({ id }: { id: number }) {
+  const [fetchTitle, setFetchTitle] = useState('')
+  const [loading, setLoading] = useState(true)
+
   const post = {
     id: id,
-    title: 'Tick Tock Clockhead',
+    title: '',
     excerpt: 'this is the dummmmmy excerpt',
     image: '/hello-kitty-wall-clock-small.png',
   }
+  useEffect(() => {
+    fetch('https://random-word-api.herokuapp.com/word?number=4&length=7')
+      .then((response) => response.json())
+      .then((data) => {
+        setFetchTitle(data.join(' '))
+        setLoading(false)
+      })
+      .catch((error) => console.error('Error:', error))
+  }, [])
+
+  post.title = loading ? post.title : fetchTitle
 
   return (
     <div className={styles['post-card-wrapper']}>
