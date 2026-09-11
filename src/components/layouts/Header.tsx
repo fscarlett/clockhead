bd95cb315baf9a705d9cdff3d5acc102a1ca4816
Fox Scarlett <foxontheone@gmail.com>
2026-09-10 20:28:30 -0700

    fix active nav styles

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,6 +1,10 @@
 import { NavLink } from 'react-router'
 import styles from '../../styles/Header.module.css'
 
+// CSS Modules hash class names, so NavLink's literal "active" class must be mapped to styles.active manually
+const navLinkClassName = ({ isActive }: { isActive: boolean }) =>
+  isActive ? `${styles['nav-link']} ${styles.active}` : styles['nav-link']
+
 function Header() {
   return (
     <header className={styles.header}>
@@ -11,23 +15,23 @@ function Header() {
               <img src='/clockhead-logo-01-small.png' alt='Clockhead Logo' />
             </NavLink>
             <nav className={styles['nav-menu']}>
-              <NavLink to='/about' className={styles['nav-link']}>
+              <NavLink to='/about' className={navLinkClassName}>
                 About Us
               </NavLink>
-              <NavLink to='/shop' className={styles['nav-link']}>
+              <NavLink to='/shop' className={navLinkClassName}>
                 Shop
               </NavLink>
-              <NavLink to='/blog' className={styles['nav-link']}>
+              <NavLink to='/blog' className={navLinkClassName}>
                 Blog
               </NavLink>
-              <NavLink to='/finder' className={styles['nav-link']}>
+              <NavLink to='/finder' className={navLinkClassName}>
                 Store Finder
               </NavLink>
             </nav>
           </div>
           <div className={styles['nav-right']}>
             <div className={styles['nav-menu']}>
-              <NavLink to='/support' className={styles['nav-link']}>
+              <NavLink to='/support' className={navLinkClassName}>
                 Support
               </NavLink>
             </div>
